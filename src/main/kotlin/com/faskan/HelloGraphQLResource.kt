373949fb960dce7b30d84edf0b8251122f2c08a9
Khan Faisal <faisal.khan@telenetgroup.be>
2024-12-05 17:02:33 +0100

    try sealed interface

--- src/main/kotlin/com/faskan/HelloGraphQLResource.kt
+++ src/main/kotlin/com/faskan/HelloGraphQLResource.kt
@@ -8,8 +8,56 @@ import org.eclipse.microprofile.graphql.Query
 @GraphQLApi
 class HelloGraphQLResource {
 
-    @Query
-    @Description("Say hello")
-    fun sayHello(@DefaultValue("World") name: String): String = "Hello $name"
+    @Query("customerProducts")
+    @Description("return all customer products")
+    fun sayHello(name: String): CustomerProductHolding {
+        return CustomerProductHolding(
+            mobileLines = listOf(
+                MobileLine.StandardMobileLine("1", "12345678"),
+                MobileLine.BundleMobileLine("2", "87654321", "1")
+            ),
+            internetLine = listOf(
+                InternetLine.StandardInternetLine("3", 100),
+                InternetLine.BundleInternetLine("4", 200, "1")
+            )
+        )
+    }
 
+}
+
+data class CustomerProductHolding(
+    val mobileLines: List<MobileLine>,
+    val internetLine: List<InternetLine>
+)
+sealed interface CustomerProduct {
+    val id: String
+}
+
+sealed interface MobileLine : CustomerProduct {
+    val phoneNumber: String
+    data class StandardMobileLine(
+        override val id: String,
+        override val phoneNumber: String
+    ) : MobileLine
+
+    data class BundleMobileLine(
+        override val id: String,
+        override val phoneNumber: String,
+        val bundleId: String
+    ) : MobileLine
+}
+
+sealed interface InternetLine : CustomerProduct {
+    val speed: Int
+
+    data class StandardInternetLine(
+        override val id: String,
+        override val speed: Int
+    ) : InternetLine
+
+    data class BundleInternetLine(
+        override val id: String,
+        override val speed: Int,
+        val bundleId: String
+    ) : InternetLine
 }
