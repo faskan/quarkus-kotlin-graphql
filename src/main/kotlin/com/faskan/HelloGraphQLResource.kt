42c0fe9e732552b6308cf348ebaf1380958b595a
Khan Faisal <faisal.khan@telenetgroup.be>
2024-12-06 13:10:12 +0100

    try sealed interface

--- src/main/kotlin/com/faskan/HelloGraphQLResource.kt
+++ src/main/kotlin/com/faskan/HelloGraphQLResource.kt
@@ -1,6 +1,5 @@
 package com.faskan
 
-import org.eclipse.microprofile.graphql.DefaultValue
 import org.eclipse.microprofile.graphql.Description
 import org.eclipse.microprofile.graphql.GraphQLApi
 import org.eclipse.microprofile.graphql.Query
@@ -10,54 +9,28 @@ class HelloGraphQLResource {
 
     @Query("customerProducts")
     @Description("return all customer products")
-    fun sayHello(name: String): CustomerProductHolding {
-        return CustomerProductHolding(
+    fun sayHello(name: String): CustomerProducts {
+        return CustomerProducts(
             mobileLines = listOf(
-                MobileLine.StandardMobileLine("1", "12345678"),
-                MobileLine.BundleMobileLine("2", "87654321", "1")
+                CustomerProduct.MobileLine("1", "12345678")
             ),
             internetLine = listOf(
-                InternetLine.StandardInternetLine("3", 100),
-                InternetLine.BundleInternetLine("4", 200, "1")
+                CustomerProduct.InternetLine("3", 100)
             )
         )
     }
 
 }
 
-data class CustomerProductHolding(
-    val mobileLines: List<MobileLine>,
-    val internetLine: List<InternetLine>
+data class CustomerProducts(
+    val mobileLines: List<CustomerProduct>,
+    val internetLine: List<CustomerProduct>
 )
 sealed interface CustomerProduct {
     val id: String
+
+    data class MobileLine(override val id: String, val phoneNumber: String) : CustomerProduct
+    data class InternetLine(override val id: String, val speed: Int) : CustomerProduct
+
 }
 
-sealed interface MobileLine : CustomerProduct {
-    val phoneNumber: String
-    data class StandardMobileLine(
-        override val id: String,
-        override val phoneNumber: String
-    ) : MobileLine
-
-    data class BundleMobileLine(
-        override val id: String,
-        override val phoneNumber: String,
-        val bundleId: String
-    ) : MobileLine
-}
-
-sealed interface InternetLine : CustomerProduct {
-    val speed: Int
-
-    data class StandardInternetLine(
-        override val id: String,
-        override val speed: Int
-    ) : InternetLine
-
-    data class BundleInternetLine(
-        override val id: String,
-        override val speed: Int,
-        val bundleId: String
-    ) : InternetLine
-}
